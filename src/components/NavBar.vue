3283260e09583cf1104bf9ef49e6eb0a5d375b40
Andrew Groe <andrewgroe.dev@gmail.com>
2021-02-13 14:02:55 -0500

    fix responsiveness for new logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,17 @@
 <template>
   <div :class="backgroundColor">
     <header class="sticky top-0 md:flex md:justify-between z-10">
+
       <div class="flex items-center justify-between px-4 py-3">
-        <div class="logo flex ">
+        <router-link to="/">
+          <div class="logo flex items-start">
           <img
             src="@/assets/logo.png"
-            class="h-12 w-12"
+            class=" logo-image sm:h-16 sm:w-16 w-12 h-12"
           />
-          <h1 class="h-8 px-1 text-4xl text-white font-logo">Clarity Title</h1>
+          <h1 class="h-8 px-4 sm:text-5xl text-4xl text-white font-logo align-middle">Clarity Title</h1>
         </div>
+        </router-link>
 
         <div class="md:hidden">
           <button
@@ -24,12 +27,13 @@
           </button>
         </div>
       </div>
+
       <div
         :class="[backgroundColor, isOpen ? 'block' : 'hidden']"
-        class="px-2 pt-2 pb-4 rounded-b md:flex md:items-end md:pb-1 md:bg-transparent md:rounded-none absolute md:relative right-0 text-white"
+        class="px-2 pt-2 pb-4 rounded-b md:flex md:items-center md:pb-1 md:bg-transparent bg-Green-primary md:rounded-none absolute md:relative right-0 text-white"
       >
         <router-link
-          class="link block mx-4"
+          class="link block lg:mx-4 md:mx-2"
           v-for="link in routes"
           :key="link.id"
           :to="link.path"
@@ -88,6 +92,11 @@ export default {
 </script>
 
 <style scoped>
+.logo-image {
+        filter: invert(100%);
+        -webkit-filter: invert(100%);
+}
+
 .link.router-link-exact-active {
   font-weight: 500;
 
@@ -136,6 +145,7 @@ export default {
 
 .link:hover > .underline {
   background-position: left bottom;
+  border: 1px solid #000;
 }
 
 .cto-btn {
