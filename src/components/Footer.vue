7b8adac87296d3d4f7a0878a29d4084eb4aaab03
Andrew Groe <andrewgroe.dev@gmail.com>
2021-02-13 14:02:08 -0500

    add content to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="bg-Green-primary">
-    <footer class="flex justify-between items-center ">
-      <h5 class="p-2 text-xl text-white">&#0169; Clarity Title LLC 2020</h5>
-      <h6 class="text-Grey-light">Developed by Andrew Groe</h6>
+  <div class="bg-Green-primary text-white">
+    <footer class="pb-4">
+      <div class="p-2 leading-relaxed">
+        <address>
+        <ul class="text-xl pt-4 pb-8">
+      <li class="font-bold">Phone: <a class="underline" href="tel:4075672409">407-567-2409</a></li>
+      <li>Fax: 407-550-7400</li>
+      <li>Email: <a class="underline" href="mailto:info@claritytitlefl.com">info@claritytitlefl.com</a></li>
+    </ul>
+    3505 Lake Lynda Drive, Suite 200 Orlando, FL 32817
+    <iframe width="480" height="480" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=3505%20Lake%20Lynda%20Drive%2C%20Suite%20200%20Orlando%2C%20FL%2032817&key=AIzaSyAO57KzwYSKXtIcJ-rjjyY9tuuomGY2UrU" allowfullscreen></iframe>
+    </address>
+    <h5 class="text-2xl text-right">&#0169; Clarity Title LLC 2020</h5>
+      </div>
     </footer>
   </div>
 </template>
@@ -14,4 +24,5 @@ export default {
 </script>
 
 <style>
+
 </style>
