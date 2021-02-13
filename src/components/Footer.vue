40783e6ae2b8c253e47af9f5ecb2ff5553369be9
Andrew Groe <andrewgroe.dev@gmail.com>
2021-02-13 14:27:51 -0500

    resize map

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="bg-Green-primary text-white">
     <footer class="pb-4">
-      <div class="p-2 leading-relaxed">
-        <address>
-        <ul class="text-xl pt-4 pb-8">
+      <div class="p-2 leading-relaxed text-xl">
+        <address class="contact-info pt-4 flex flex-col md:flex-row md:justify-between">
+        <ul class="md:pb-8 pb:4">
       <li class="font-bold">Phone: <a class="underline" href="tel:4075672409">407-567-2409</a></li>
       <li>Fax: 407-550-7400</li>
       <li>Email: <a class="underline" href="mailto:info@claritytitlefl.com">info@claritytitlefl.com</a></li>
     </ul>
-    3505 Lake Lynda Drive, Suite 200 Orlando, FL 32817
-    <iframe width="480" height="480" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=3505%20Lake%20Lynda%20Drive%2C%20Suite%20200%20Orlando%2C%20FL%2032817&key=AIzaSyAO57KzwYSKXtIcJ-rjjyY9tuuomGY2UrU" allowfullscreen></iframe>
+    <div class="pt-4">
+    3505 Lake Lynda Drive, Suite 200
+    <br>
+    Orlando, FL 32817
+    <iframe width="360" height="360" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=3505%20Lake%20Lynda%20Drive%2C%20Suite%20200%20Orlando%2C%20FL%2032817&key=AIzaSyAO57KzwYSKXtIcJ-rjjyY9tuuomGY2UrU" allowfullscreen></iframe>
+    </div>
     </address>
     <h5 class="text-2xl text-right">&#0169; Clarity Title LLC 2020</h5>
       </div>
@@ -24,5 +28,7 @@ export default {
 </script>
 
 <style>
+.contact-info{
 
+}
 </style>
